081e178c1e3618deee96f120e775621628df1de7
iksanbaigofik <92073442+iksanbaigofik@users.noreply.github.com>
2021-11-13 19:12:04 +0700

    Add files via upload

---
+++ ListrikStatisdanDinamis.java
@@ -0,0 +1,14 @@
+package com.example.fisikakumudah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListrikStatisdanDinamis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listrikstatis_dinamis);
+    }
+}
